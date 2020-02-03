515b38e9e018a591f4f9d95c2c9cf6868b324c36
chris tregaskis <chrisProjects@Hanans-MacBook-Pro.local>
2020-02-03 15:09:22 +0000

    created basic structure of html element connecting style sheet, google font and meta device width

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en-gb">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wine Collection!</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400,600&display=swap" rel="stylesheet">
+
+</head>
+
+
+</html>
