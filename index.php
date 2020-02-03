af500eca30ae72ccd300b43be2268f6b75fb5ed0
chris tregaskis <chrisProjects@Hanans-MacBook-Pro.local>
2020-02-03 15:16:51 +0000

    completed head element

--- index.php
+++ index.php
@@ -13,10 +13,16 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Wine Collection!</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="queries.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300i,400,600&display=swap" rel="stylesheet">
 
 </head>
+<body>
+    
 
+
+</body>
 
 </html>
