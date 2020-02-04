fcad88bdfaf0ef0b3fc30454eef7ec51fb0b8bca
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-04 19:56:09 +0000

    first attempt at conecting db

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+
+function getWines($db)
+{
+    $query = $db->prepare();
+    $query->execute();
+    return $query->fetchAll();
+}
+
+function var_dumpPre($variabletoDump) {
+    echo '<pre>';
+    var_dump($variabletoDump);
+    echo '</pre>';
+}
