40697837f41d0a82e662d2f3f577ad493dde26a2
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-04 22:07:10 +0000

    completed the displayWines function however failure keeps showing on front end with keys being empty. When var_dumped keys have data, so not sure whats happening

--- functions.php
+++ functions.php
@@ -7,6 +7,12 @@ function getWines($db)
     return $query->fetchAll();
 }
 
+function var_dump_pre($a) {
+    echo '<pre>';
+    var_dump($a);
+    echo '</pre>';
+}
+
 function displayWines(array $wines): string
 {
     if (!array_key_exists('name', $wines) ||
@@ -22,5 +28,32 @@ function displayWines(array $wines): string
         return 'error! array keys missing from displayWines function';
     }
 
-
+    $result = '';
+    foreach ($wines as $wine)
+    {
+        $result = '<article class=\'wine-item\'>';
+        $result .= '<div class=\'wine-img\' style=\'background-image: url(' . 'Resources/' . $wine['img'] . ')\'></div>';
+        $result .= '<div class="content">';
+        $result .= '<h3>' . $wine['name'] . '</h3>';
+        $result .= '<ul>';
+        $result .= '<li><span>Year: </span>' . $wine['year'] . '</li>';
+        $result .= '<li><span>Origin: </span>' . $wine['origin'] . '</li>';
+        $result .= '<li><span>Profile: </span>' . $wine['profile'] . '</li>';
+        $result .= '<li><span>Body: </span>' . $wine['body'] . '</li>';
+        $result .= '<li><span>ABV: </span>' . $wine['abv'] . '</li>';
+        $result .= '<li><span>Cheese: </span>' . $wine['cheese'] . '</li>';
+        $result .= '</ul>';
+        $result .= '<div class=\'view-more-link\'><a href=\'' . $wine['link'] . '\'>View More</a></div>';
+        $result .= '</div>';
+        $result .= '</article>';
+    }
+    return $result;
 }
+
+
+
+
+
+
+
+
