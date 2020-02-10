df4e97b9a3954bd008b6f8b2a289ef368debf377
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-10 20:29:58 +0000

    amended displayWines function to only display wines where hidden == false

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  * @return mixed, returns assoc array of required data
  */
 function getWines($db) {
-    $query = $db->prepare('SELECT `id`, `name`, `year`, `origin`, `profile`, `body`, `abv`, `cheese`, `link`, `img` FROM `wines`;');
+    $query = $db->prepare('SELECT `id`, `name`, `year`, `origin`, `profile`, `body`, `abv`, `cheese`, `link`, `img`, `hidden` FROM `wines`;');
     $query->execute();
     return $query->fetchAll();
 }
@@ -43,7 +43,7 @@ function keysExist(array $wines) : bool {
     return $result;
 }
 
-/** Display each wine from assoc array
+/** Display each wine from assoc array if not set to hidden
  *
  * @param array $wines, takes an associated array
  * @return string, returns the html code that gets echo'ed out
@@ -52,26 +52,29 @@ function displayWines(array $wines): string {
     $result = '';
     foreach ($wines as $wine)
     {
-        $result .= '<article class=\'wine-item\'>';
-        $result .= '<div class=\'wine-img\' style=\'background-image: url(' . 'Resources/' . $wine['img'] . ')\'></div>';
-        $result .= '<div class="content">';
-        $result .= '<h3>' . $wine['name'] . '</h3>';
-        $result .= '<ul>';
-        $result .= '<li><span>Year: </span>' . $wine['year'] . '</li>';
-        $result .= '<li><span>Origin: </span>' . $wine['origin'] . '</li>';
-        $result .= '<li><span>Profile: </span>' . $wine['profile'] . '</li>';
-        $result .= '<li><span>Body: </span>' . $wine['body'] . '</li>';
-        $result .= '<li><span>ABV: </span>' . $wine['abv'] . '</li>';
-        $result .= '<li><span>Cheese: </span>' . $wine['cheese'] . '</li>';
-        $result .= '</ul>';
-        $result .= '<div class=\'view-more-link\'><a href=\'' . $wine['link'] . '\'>View More</a></div>';
-        $result .= '<div class=\'delete-wine-btn\'><a href=\'#\'' . '>Delete Wine</a></div>';
-        $result .= '</div>';
-        $result .= '</article>';
+        if ($wine['hidden'] == 0) {
+            $result .= '<article class=\'wine-item\'>';
+            $result .= '<div class=\'wine-img\' style=\'background-image: url(' . 'Resources/' . $wine['img'] . ')\'></div>';
+            $result .= '<div class="content">';
+            $result .= '<h3>' . $wine['name'] . '</h3>';
+            $result .= '<ul>';
+            $result .= '<li><span>Year: </span>' . $wine['year'] . '</li>';
+            $result .= '<li><span>Origin: </span>' . $wine['origin'] . '</li>';
+            $result .= '<li><span>Profile: </span>' . $wine['profile'] . '</li>';
+            $result .= '<li><span>Body: </span>' . $wine['body'] . '</li>';
+            $result .= '<li><span>ABV: </span>' . $wine['abv'] . '</li>';
+            $result .= '<li><span>Cheese: </span>' . $wine['cheese'] . '</li>';
+            $result .= '</ul>';
+            $result .= '<div class=\'view-more-link\'><a href=\'' . $wine['link'] . '\'>View More</a></div>';
+            $result .= '<div class=\'delete-wine-btn\'><a href=\'#\'' . '>Delete Wine</a></div>';
+            $result .= '</div>';
+            $result .= '</article>';
+        }
     }
     return $result;
-
 }
+
+
 
 /** Check the string input is greater than 0 and less than 250
  *
