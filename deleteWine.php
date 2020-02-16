f7c169a3f1cdace42696e916f9a1578ebb204b6b
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-02-16 07:03:11 +0000

    delete process successfully pulling HARDCODED input from displayFunction and sending correct data to deleteWine function. Successfully updating db

--- deleteWine.php
+++ deleteWine.php
@@ -3,12 +3,16 @@
 require_once 'connectDB.php';
 require_once 'functions.php';
 
+
 //if delete pressed, call delete function and return to index.php
 if(isset($_POST['delete-wine'])) {
 
-    connectDB();
+    $wine_id = $_POST['wine-id'];
 
-    //deleteWine()
+    $db = connectDB();
+
+    deleteWine($wine_id, $db);
+
+    header('Location: index.php');
 
 }
-
